cd53a2faf2c53848c7c71eaa513c9864df406f4e
xavierjs <xavier.bourry@polytechnique.org>
2023-03-14 12:36:40 -0400

    [NN] Upgrade neural network for Wrist back side VTO. New one really rocks

--- demos/VTO/main.js
+++ demos/VTO/main.js
@@ -5,7 +5,7 @@ const NNPath = '../../neuralNets/';
 
 const NNWristVersion = '27';
 const NNRingVersion = '14'; //*/
-const NNWristBackVersion = '1';
+const NNWristBackVersion = '7';
 const NNRingBackVersion = '6';
 
 const wristModesCommonSettings = {
